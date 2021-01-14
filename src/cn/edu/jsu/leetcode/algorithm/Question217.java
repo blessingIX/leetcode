c669196031b1c9554812031e6cdc9b1054be60f9
blessingIX <1350437095@qq.com>
2021-01-14 21:28:06 +0800

    栈：496；哈希表：217；

---
+++ src/cn/edu/jsu/leetcode/algorithm/Question217.java
@@ -0,0 +1,61 @@
+package cn.edu.jsu.leetcode.algorithm;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+/**
+ * @description:
+ * @author: blessing
+ * @create: 2021-01-14 17:36
+ */
+public class Question217 {
+//    https://leetcode-cn.com/problems/contains-duplicate/
+    public boolean containsDuplicate(int[] nums) {
+//        https://leetcode-cn.com/submissions/detail/138392199/
+        if (nums == null) return false;
+        HashSet<Integer> hashSet = new HashSet<>();
+        for (int num : nums) {
+            if (hashSet.contains(num)) {
+                return true;
+            }
+            hashSet.add(num);
+        }
+        return false;
+    }
+
+    public boolean containsDuplicate1ms(int[] nums) {
+//        ???
+        if (nums == null || nums.length == 0){
+            return false;
+        }
+        int temp = nums[0];
+        for (int i = 1 ; i < nums.length ; i++){
+            if (nums[i] == temp){
+                return true;
+            } else if (nums[i] > temp){
+                temp = nums[i];
+            } else {
+                for (int j = i - 1 ; j >= 0 ; j--){
+                    if (nums[i] == nums[j]){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Question217 question = new Question217();
+        int[][] cases = new int[][]{
+                {1,2,3,1},
+                {1,2,3,4},
+                {1,1,1,3,3,4,3,2,4,2}
+        };
+
+        for (int[] aCase : cases) {
+//            System.out.println(Arrays.toString(aCase) + " => " + question.containsDuplicate(aCase));
+            System.out.println(Arrays.toString(aCase) + " => " + question.containsDuplicate1ms(aCase));
+        }
+    }
+}
---
+++ src/cn/edu/jsu/leetcode/algorithm/Question496.java
@@ -0,0 +1,72 @@
+package cn.edu.jsu.leetcode.algorithm;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+/**
+ * @description: 496. 下一个更大元素 I
+ * @author: blessing
+ * @create: 2021-01-14 10:42
+ */
+public class Question496 {
+//    https://leetcode-cn.com/problems/next-greater-element-i/
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] result = new int[nums1.length];
+        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> temp = new Stack<>();
+        for (int i : nums2) {
+            stack.push(i);
+        }
+        for (int i = 0; i < nums1.length; i++) {
+            int max = -1;
+            boolean isFound = false;
+            while (!stack.isEmpty() && !isFound) {
+                int top = stack.pop();
+                if (top > nums1[i]) max = top;
+                else if (top == nums1[i]) isFound = true;
+                temp.push(top);
+            }
+            result[i] = max;
+            while (!temp.isEmpty()) {
+                stack.push(temp.pop());
+            }
+        }
+        return result;
+    }
+
+    public int[] nextGreaterElementTraverse(int[] nums1, int[] nums2) {
+        int[] result = new int[nums1.length];
+        int len = nums2.length;
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = -1;
+            for (int j = len - 1; j >= 0; j--) {
+                if (nums2[j] > nums1[i]) result[i] = nums2[j];
+                if (nums2[j] == nums1[i]) break;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Question496 question = new Question496();
+        int[][][] cases = new int[][][]{
+                {
+                        {4,1,2}, {1,3,4,2}
+                },
+                {
+                        {2,4},{1,2,3,4}
+                },
+                {
+                        {4,1,2},
+                        {1,2,3,4}
+                }
+        };
+        int i = 1;
+        for (int[][] aCase : cases) {
+            System.out.printf("=====case%d=====\n", i++);
+            System.out.println("nums1 = " + Arrays.toString(aCase[0]));
+            System.out.println("nums2 = " + Arrays.toString(aCase[1]));
+            System.out.println(Arrays.toString(question.nextGreaterElementTraverse(aCase[0], aCase[1])));
+        }
+    }
+}
